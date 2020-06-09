683a69914c9921d10e39103e60deb611c63d5da6
Grant Dempsey <CryptoDemps@gmail.com>
2020-06-09 04:31:43 -0600

    all enigma tests passing with new setup

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,12 +9,6 @@ class Enigma
     @decryption = Decryption.new
   end
 
-  # attr_reader :alphabet
-
-  # def initialize
-  #   @alphabet = ("a".."z").to_a << " "
-  # end
-
   def date_today
     Time.now.strftime("%d%m%y")
   end
@@ -22,73 +16,13 @@ class Enigma
   def rand_number
     4.times.map { (0..9).to_a.sample }.join.rjust(5, "0")
   end
-  
-  # def split_key(key)
-  #   key_array = []
-  #   key.chars.each_cons(2){|chars| key_array << chars.join}
-  #   key_array.map { |char| char.to_i }
-  # end
-  #
-  # def offsets(date)
-  #   (date.to_i ** 2).to_s.chars.last(4).map { |char| char.to_i }
-  # end
-  #
-  # def shifts_date_and_key(key, date)
-  #   split_key(key).zip(offsets(date)).map { |nums| nums.reduce(:+) }
-  # end
-  #
-  # def split_message(message)
-  #   message.chars.each_slice(4).to_a.map do |chars|
-  #     chars.map { |char| char.downcase }
-  #   end
-  # end
 
-  # def encrypt(message, key = rand_number, date = date_today)
-  #  {
-  #    encryption: encrypt_m(message, key, date),
-  #    key: key,
-  #    date: date
-  #  }
-  # end
+  def encrypt(message, key = rand_number, date = date_today)
+   @encryption.encrypt(message, key, date)
+  end
 
-  # def decrypt(encrypted_message, key, date = date_today)
-  #  {
-  #    decryption: decrypt_m(encrypted_message, key, date),
-  #    key: key,
-  #    date: date
-  #  }
-  # end
-
-  # def encrypt_m(message, key = rand_number, date = date_today)
-  #   encrypted = ""
-  #   shifts = shifts_date_and_key(key, date)
-  #   split_message(message).each do |chars|
-  #     chars.zip(shifts).each do |char, shift_value|
-  #       if alphabet.include?(char)
-  #         new_index = (@alphabet.find_index(char) + shift_value) %27
-  #         encrypted.concat(@alphabet[new_index])
-  #       else
-  #         encrypted.concat(char)
-  #       end
-  #     end
-  #   end
-  #   encrypted
-  # end
-
-  # def decrypt_m(message, key = rand_number, date = date_today)
-  #   decrypted = ""
-  #   shifts = shifts_date_and_key(key, date)
-  #   split_message(message).each do |chars|
-  #     chars.zip(shifts).each do |char, shift_value|
-  #       if alphabet.include?(char)
-  #         new_index = (@alphabet.find_index(char) - shift_value) %27
-  #         decrypted.concat(@alphabet[new_index])
-  #       else
-  #         decrypted.concat(char)
-  #       end
-  #     end
-  #   end
-  #   decrypted
-  # end
+ def decrypt(encrypted_message, key, date = date_today)
+   @decryption.decrypt(encrypted_message, key, date)
+ end
 
 end
--- test/test_enigma.rb
+++ test/test_enigma.rb
@@ -22,4 +22,75 @@ class EnigmaTest < Minitest::Test
     assert_equal "0", enigma.rand_number[0]
   end
 
+  def test_encrypt_with_key_and_date
+    enigma = Enigma.new
+    expected = { encryption: "keder ohulw",
+                key: "02715",
+                date: "040895" }
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_decrypt_message_with_key_and_date
+    enigma = Enigma.new
+    expected = { decryption: "hello world",
+                 key: "02715",
+                 date: "040895" }
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
+  def test_encrypt_with_key_no_date
+    enigma = Enigma.new
+    expected = { encryption: "keder ohulw",
+                key: "02715",
+                date: "040895" }
+    enigma.stubs(:date_today).returns("040895")
+    assert_equal expected, enigma.encrypt("hello world", "02715")
+  end
+
+  def test_decrypt_with_key_no_date
+    enigma = Enigma.new
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+    enigma.stubs(:date_today).returns("040895")
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715")
+  end
+
+  def test_encrypt_with_a_random_key_and_todays_date
+     enigma = Enigma.new
+     enigma.stubs(:date_today).returns("040895")
+     enigma.stubs(:rand_number).returns("02715")
+     expected = {
+       encryption: "keder ohulw",
+       key: "02715",
+       date: "040895"
+     }
+     assert_equal expected, enigma.encrypt("hello world")
+  end
+
+  def test_decrypt_with_characters_not_in_alphabet_and_capital_letters
+    enigma = Enigma.new
+    enigma.stubs(:date_today).returns("040895")
+    enigma.stubs(:rand_number).returns("02715")
+    expected = {
+      encryption: "keder ohulw!",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.encrypt("Hello World!")
+  end
+
+  def test_decrypt_with_characters_not_in_alphabetand_capital_letters
+    enigma = Enigma.new
+    enigma.stubs(:date_today).returns("040895")
+    expected = {
+      decryption: "hello world!",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.decrypt("Keder Ohulw!", "02715")
+  end
+
 end
